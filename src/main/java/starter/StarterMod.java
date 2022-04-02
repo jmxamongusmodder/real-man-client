291a76bc6e1560695cfd92be06522a4dccc7e8fd
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-04-01 23:56:18 -0400

    begginings

--- src/main/java/starter/StarterMod.java
+++ src/main/java/starter/StarterMod.java
@@ -1,14 +1,28 @@
 package starter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.lwjgl.glfw.GLFW;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.client.util.InputUtil;
 
 public class StarterMod implements ModInitializer {
+
+	public static final String VERSION = "7.22";
+	public static final String MC_VERSION = "1.18.2";
+
   @Override
   public void onInitialize() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
-    System.out.println("Hello Fabric world!");
+       System.out.println("The world of real man has loaded! enjoy your stay!");
   }
 }
